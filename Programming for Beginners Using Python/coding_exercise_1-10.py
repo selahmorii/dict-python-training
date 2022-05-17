4a8d67983df9d98f1371ab4473828d1206c2e7ed
Jozy Francisco <jzfrancisco95@gmail.com>
2022-05-17 22:22:32 +0800

    var_stringcon
    learn to use variable and string concatenation

---
+++ Programming for Beginners Using Python/coding_exercise_1-10.py
@@ -0,0 +1,15 @@
+char_job = "pirate"
+char_ride = "ship"
+char_souvenir = "gold"
+char_pet = "parrot"
+
+print("There was once a "+ char_job +" who loved adventure.")
+print("The "+ char_job +" would take her "+ char_ride +" to unknown places.")
+print("She brings home a lot of "+ char_souvenir +".")
+print("Then she goes home to her pet "+ char_pet +", Chuckles.")
+
+# OUTPUT:
+# There was once a pirate who loved adventure.
+# The pirate would take her ship to unknown places.
+# She brings home a lot of gold.
+# Then she goes home to her pet parrot, Chuckles.
