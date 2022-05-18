883e982c61c5787df47154de8e3b4c08b6e34aaf
Jozy Francisco <jzfrancisco95@gmail.com>
2022-05-18 22:19:55 +0800

    learn debugging

---
+++ Programming for Beginners Using Python/coding_exercise_1-14.py
@@ -0,0 +1,22 @@
+# Coding Exercise 1.14
+    # this exercise is more on debugging (?) or checking for errors
+
+#1) printRoses are red, violets are blue
+print("Roses are red, violets are blue")
+    #inserted parenthesis and quoatation marks to indicate string
+
+''' 2)
+if 10 > 7:
+print ("Ten is greater than seven!")
+if 16 < 42:
+print("Sixteen is less than forty-two!")
+'''
+if 10 > 7:
+    print ("Ten is greater than seven!")
+if 16 < 42: 
+    print("Sixteen is less than forty-two!")
+    #indentation
+
+#3) print("A long time ago in a galaxy far, far away...");
+print("A long time ago in a galaxy far, far away...")
+    #removed semi-colon
