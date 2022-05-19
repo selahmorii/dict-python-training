fc8e617d569784f46edb64698b9f1ae80b20b359
Jozy Francisco <jzfrancisco95@gmail.com>
2022-05-19 22:35:52 +0800

    making slambook

---
+++ Programming for Beginners Using Python/coding_exercise_2-7.py
@@ -0,0 +1,31 @@
+# making a slambook
+
+name=input("What's your name?")
+age=int(input("How old are you?"))
+color=input("What's your fave color?")
+movie=input("What's your fave movie?")
+mobile=int(input("What is your mobile number?"))
+motto=input("What's your motto in life?")
+print("\n")
+print("My name is " , name + ". I'm " , str(age) , "years old.")
+print("My favorites in terms of color, and movie are ", color +", " + movie +", respectively.")
+print("My mobile number is "+ str(mobile) +".")
+print("My motto in life is "+ motto +".")
+
+
+'''
+OUTPUT:
+
+What's your name?Jozy
+How old are you?27
+What's your fave color?Red
+What's your fave movie?Up
+What is your mobile number?123456789
+What's your motto in life?Time is gold
+
+
+My name is  Jozy. I'm  27 years old.
+My favorites in terms of color, and movie are  Red, Up, respectively.
+My mobile number is 123456789.
+My motto in life is Time is gold.
+'''
