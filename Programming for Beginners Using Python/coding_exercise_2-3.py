df155a77df28c58154d38864f8f98187290a9615
Jozy Francisco <jzfrancisco95@gmail.com>
2022-05-19 21:48:12 +0800

    def fxn_parameter_argument

---
+++ Programming for Beginners Using Python/coding_exercise_2-3.py
@@ -0,0 +1,12 @@
+'''
+1) Use the def keyword to create a function that will say, "Have a nice day!".
+2) Set a parameter called name. Remember to use the print () function and string concatenation.
+3) Call your function. Set an argument to put within the function's parentheses.
+'''
+
+def greetings(name):
+    print("Have a nice day, " + name +"!")
+
+greetings("Maki")
+
+# OUTPUT: Have a nice day, Maki!
