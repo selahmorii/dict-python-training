e20209562c43da8da155719be9bfdba36496c8e7
Jozy Francisco <jzfrancisco95@gmail.com>
2022-05-23 21:47:53 +0800

    data types

---
+++ Programming for Beginners Using Python/coding_exercise_2-11.py
@@ -0,0 +1,20 @@
+# 1) Create a boolean statement to determine whether 10 is greater than seven
+# 2) Convert the numerical value 73911 into a string
+# 3) Convert the string "Thank God it's Friday into a tuple
+# 4) Convert 4302 into a floating number
+# 5) Convert 3299.35640 into an integer.
+
+print(10>7)
+print(str(73911))
+print(tuple("Thank God it's Friday!"))
+print(float(4302))
+print(int(3299.35640))
+
+'''
+OUTPUT:
+True
+73911
+('T', 'h', 'a', 'n', 'k', ' ', 'G', 'o', 'd', ' ', 'i', 't', "'", 's', ' ', 'F', 'r', 'i', 'd', 'a', 'y', '!')
+4302.0
+3299
+'''
