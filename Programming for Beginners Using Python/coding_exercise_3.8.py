a025738eaa287b244f4c859ca9624d3cd881f213
Jozy Francisco <jzfrancisco95@gmail.com>
2022-05-28 17:53:31 +0800

    arithmetic

---
+++ Programming for Beginners Using Python/coding_exercise_3.8.py
@@ -0,0 +1,16 @@
+# Arithmetic Coding Exercise
+
+print(217 * 6) #multiplication
+print(35234 + 600) #addition
+print(67 // 12) #floor division
+print(56329 % 982) #modulus
+print(34 ** 5) #exponentiation
+
+'''
+OUTPUT:
+1302
+35834
+5
+355
+45435424
+'''
