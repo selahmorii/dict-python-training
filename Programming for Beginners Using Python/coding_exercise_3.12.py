85c7892190e5b1de1126ab96e4765593954db01e
Jozy Francisco <jzfrancisco95@gmail.com>
2022-05-28 18:46:04 +0800

    logical_operational
    relational operators

---
+++ Programming for Beginners Using Python/coding_exercise_3.12.py
@@ -0,0 +1,20 @@
+#logical and operational operators
+
+my_age = 22
+mom_age = 61
+sister_age = 29
+
+print(mom_age < sister_age and my_age == 22)
+print(mom_age == 61)
+print(mom_age > 34 or sister_age == 22)
+print(mom_age >= 54)
+print(not(sister_age <= 400 and my_age == 22))
+
+'''
+OUTPUT:
+False
+True
+True
+True
+False
+'''
