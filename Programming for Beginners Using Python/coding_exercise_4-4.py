5d5d52ac5318b926e59a895debe38b3b1e5729b0
Jozy Francisco <jzfrancisco95@gmail.com>
2022-05-28 19:17:39 +0800

    conditional statements
    if, else, elif

---
+++ Programming for Beginners Using Python/coding_exercise_4-4.py
@@ -0,0 +1,16 @@
+# IF, ELSE, ELIF
+
+x = 332
+y = 2031
+
+if x >= y:
+    print("x is greater than or equal to y.")
+elif x == y:
+    print("x is equal to y.")
+else:
+    print("x is less than y.")
+
+'''
+OUTPUT:
+x is less than y.
+'''
